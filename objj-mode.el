2762324696a36ec3ce1ed3093af1a658aee30e99
Thomas Brown <tabsoftwareconsulting@gmail.com>
2017-10-15 09:25:00 -0400

    Implement without use-package.

--- objj-mode.el
+++ objj-mode.el
@@ -1,22 +1,19 @@
 ;;; Objective-J programming language
-(use-package objc-mode
-  :preface
+(eval-after-load "cc-mode.el"
   (defconst cappuccino-objj-style
     '((c-basic-offset . 4)
       (tab-width . 8)
       (indent-tabs-mode . nil)
       (c-offsets-alist
        (substatement-open . 0))))
-  :mode
-  (("\\.j\\'" . objj-mode))
-  :init
+
   (define-derived-mode objj-mode objc-mode
     "Objective-J"
     "Major mode for editing Objective-J files.")
-  (use-package flycheck
-    :ensure t
-    :pin melpa
-    :config
+
+  (add-to-list 'auto-mode-alist '("\\.j\\'" . objj-mode))
+
+  (eval-after-load "flycheck.el"
     (flycheck-define-checker objj-capp-lint
       "A flycheck checker for Objective-J based on capp_lint."
       :command
@@ -25,18 +22,19 @@
       ((warning line-start line ": " (message) "." line-end))
       :modes
       (objj-mode))
+
     (add-to-list 'flycheck-checkers 'objj-capp-lint)
     (add-hook 'objj-mode-hook (lambda ()
                                 (flycheck-select-checker 'objj-capp-lint))))
-  (use-package compile
-    :config
+
+  (eval-after-load "compile.el"
     (add-to-list 'compilation-error-regexp-alist-alist
                  '(objj-acorn "^\\(WARNING\\|ERROR\\) line \\([0-9]+\\) in file:\\([^:]+\\):\\(.*\\)$" 3 2))
-    (add-to-list 'compilation-error-regexp-alist 'objj-acorn)))
-; @todo this should be up in use-package, but are not being called
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (c-add-style "cappuccino" cappuccino-objj-style t)))
-(add-hook 'objj-mode-hook
-          (lambda ()
-            (c-set-style "cappuccino")))
+    (add-to-list 'compilation-error-regexp-alist 'objj-acorn))
+
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (c-add-style "cappuccino" cappuccino-objj-style t)))
+  (add-hook 'objj-mode-hook
+            (lambda ()
+              (c-set-style "cappuccino"))))
